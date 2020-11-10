452912bd778f119a03eaf5008634cdf008527789
songping <songp@yiche.com>
2020-11-10 11:54:35 +0800

    add README

---
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/yiche/myapplication/HandlerActivity.java
@@ -0,0 +1,13 @@
+package com.yiche.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HandlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handler);
+    }
+}
---
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/yiche/myapplication/TestActivity.java
@@ -0,0 +1,10 @@
+package com.yiche.myapplication;
+
+/**
+ * Created on 2019-09-30 11:28.
+ *
+ * @author songp
+ * Email: songp@yiche.com
+ */
+public class TestActivity extends MainActivity {
+}
